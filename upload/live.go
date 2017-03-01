74fa611c0236d086d9c5db09ec97bb1ca33c99f7
rainfly <xiechc@gmail.com>
2017-03-01 16:06:46 +0800

    ...

--- upload/live.go
+++ upload/live.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	//"io/ioutil"
 	//"net/http"
 	//	"os"
@@ -9,9 +9,45 @@ import (
 	//"path"
 	"strings"
 	//"time"
+	"image"
+	"image/jpeg"
+	"os"
+	"path"
 )
 
-func Checkvideo(origin string) {
+func WidthHeightFile(picture string) string {
+	total := len(picture)
+	if total < 5 {
+		return ""
+	}
+	var img image.Image
+	file, err := os.Open(picture)
+	if err != nil {
+		return ""
+	}
+
+	img, err = jpeg.Decode(file)
+	file.Close()
+
+	bounds := img.Bounds()
+	min, max := bounds.Min, bounds.Max
+	height, width := max.Y-min.Y, max.X-min.X
+
+	index := strings.LastIndex(picture, ".")
+	ext := path.Ext(picture)
+	var temp string = fmt.Sprintf("%s-%dx%d%s", picture[:index], width, height, ext)
+	out, err := os.Create(temp)
+	if err != nil {
+		return path.Base(picture)
+	}
+	// write new image to file
+	jpeg.Encode(out, img, nil)
+	out.Close()
+	//os.Rename(temp, name)
+	return path.Base(temp)
+}
+
+func Checkvideo(origin string) string {
 
 	index := strings.LastIndex(origin, ".")
 	if index > 0 {
@@ -22,14 +58,20 @@ func Checkvideo(origin string) {
 		//cmd.Stdout = os.Stdout
 		//cmd.Stderr = os.Stderr
 		err := cmd.Run()
-		logger.Println(err)
+		fmt.Println(err)
+		return dest
 	}
+	return ""
 }
 
-func Check_Thread() {
-	for video := range Channel {
-		go Checkvideo(video)
-	}
-}
+//func Check_Thread() {
+//	for video := range Channel {
+//		go Checkvideo(video)
+//	}
+//}
 
-var Channel chan string
+//var Channel chan string
+
+/*func main() {
+	fmt.Println(WidthHeightFile(os.Args[1]))
+}*/
