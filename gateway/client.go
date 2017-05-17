e8222a505191ea61f76fc9215ea1509985923d30
rainfly <xiechc@gmail.com>
2017-05-17 14:56:57 +0800

    gatwayg

--- gateway/client.go
+++ gateway/client.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net"
-	"os"
+	"time"
 )
 
-func main() {
-	if num := len(os.Args); num < 2 {
-		fmt.Println("no bindip")
-		os.Exit(-1)
+type Transmit struct {
+	RemoteIPAddr string
+	Userid       string
+	Message      string
+}
+
+const LocalIPAddr = "192.168.1.240:1111"
+
+var udp_addr *net.UDPAddr
+var conn *net.UDPConn
+var ReceiveChan chan Transmit
+
+func SendOut(temp Transmit, remote string) {
+
+	dudp_addr, _ := net.ResolveUDPAddr("udp4", remote)
+
+	var network bytes.Buffer
+	enc := gob.NewEncoder(&network)
+	err2 := enc.Encode(temp)
+	if err2 == nil {
+		conn.WriteToUDP(network.Bytes(), dudp_addr)
 	}
-	udp_addr, _ := net.ResolveUDPAddr("udp", "192.168.1.240:1111")
-	conn, _ := net.ListenUDP("udp", udp_addr)
 
-	dudp_addr, _ := net.ResolveUDPAddr("udp", "192.168.1.240:2222")
-	conn.WriteToUDP([]byte("Hello world!"), dudp_addr)
+	//var msg [20]byte
+	//conn.Read(msg[0:])
+}
 
-	var msg [20]byte
-	conn.Read(msg[0:])
+func InitUDPSend() {
+	udp_addr, _ = net.ResolveUDPAddr("udp", LocalIPAddr)
+	conn, _ = net.ListenUDP("udp", udp_addr)
+	ReceiveChan = make(chan Transmit, 200)
+	fmt.Println("UDPSend inited...")
+	go func() {
+		for temp := range ReceiveChan {
+			SendOut(temp, temp.RemoteIPAddr)
+			fmt.Println(temp)
+		}
+	}()
+}
 
-	fmt.Println("msg is", string(msg[0:10]))
+func main() {
+	var temp = Transmit{"192.168.1.240:2222", "100000320", "video_touser_from啊地方"}
+	InitUDPSend()
+	for {
+		ReceiveChan <- temp
+		time.Sleep(1 * time.Second)
+	}
 }
---
+++ gateway/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+)
+
+const LocalIPAddr = "192.168.1.240:2222"
+
+type Transmit struct {
+	RemoteIPAddr string
+	Userid       string
+	Message      string
+}
+
+func main() {
+	udp_addr, err := net.ResolveUDPAddr("udp", LocalIPAddr)
+	checkError(err)
+
+	conn, err := net.ListenUDP("udp", udp_addr)
+	defer conn.Close()
+	checkError(err)
+
+	//go recvUDPMsg(conn)
+	for {
+		var buf [512]byte
+
+		n, raddr, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			return
+		}
+
+		//fmt.Println("msg is ", string(buf[0:n]))
+		fmt.Println("addr is ", raddr.String())
+		temp := bytes.NewBuffer(buf[:n])
+		dec := gob.NewDecoder(&*temp)
+		var v Transmit
+		err = dec.Decode(&v)
+		if err == nil {
+			fmt.Println(v)
+		}
+		//_, err = conn.WriteToUDP([]byte("nice to see u"), raddr)
+		//checkError(err)
+
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Error: %s", err.Error())
+		os.Exit(1)
+	}
+}
