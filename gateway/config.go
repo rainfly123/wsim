f2f8ca9485cc9442fae11400e4778733ba95164a
Your Name <you@example.com>
2017-05-27 11:47:39 +0800

    redis ip

--- gateway/config.go
+++ gateway/config.go
@@ -1,7 +1,7 @@
 package gateway
 
-const LocalIPRecvAddr = "192.168.1.240:2222"
-const LocalIPSendAddr = "192.168.1.240:1111"
+const LocalIPRecvAddr = "10.116.129.117:2222"
+const LocalIPSendAddr = "10.116.129.117:1111"
 
 type Transmit struct {
 	RemoteIPAddr string
@@ -10,5 +10,5 @@ type Transmit struct {
 }
 
 //redis
-const host = "127.0.0.1"
+const host = "10.169.205.202"
 const port = uint(6379)
